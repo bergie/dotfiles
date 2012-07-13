e3483362db1ef3f7509a189493bfe9aa898b31c0
Henri Bergius <henri.bergius@iki.fi>
2012-07-13 15:41:50 +0200

    Get rid of Caps Lock

--- puppet.pp
+++ puppet.pp
@@ -1,20 +1,24 @@
 file { "/home/${id}/.vimrc":
- ensure => link,
- target => "/home/${id}/Projects/dotfiles/vimrc",
+  ensure => link,
+  target => "/home/${id}/Projects/dotfiles/vimrc",
 }
 file { "/home/${id}/.vim":
- ensure => link,
- target => "/home/${id}/Projects/dotfiles/vim",
+  ensure => link,
+  target => "/home/${id}/Projects/dotfiles/vim",
 }
 file { "/home/${id}/.gitconfig":
- ensure => link,
- target => "/home/${id}/Projects/dotfiles/gitconfig",
+  ensure => link,
+  target => "/home/${id}/Projects/dotfiles/gitconfig",
 }
 file { "/home/${id}/.zshrc":
- ensure => link,
- target => "/home/${id}/Projects/dotfiles/zshrc",
+  ensure => link,
+  target => "/home/${id}/Projects/dotfiles/zshrc",
 }
 file { "/home/${id}/.tmux.conf":
   ensure => link,
   target => "/home/${id}/Projects/dotfiles/tmux.conf",
+}
+file { "/home/${id}/.Xmodmap":
+  ensure => link,
+  target => "/home/${id}/Projects/dotfiles/Xmodmap",
 }
