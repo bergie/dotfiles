7a5fc7235779e0cbc8c67d591f2b5586f55ab76b
Henri Bergius <henri.bergius@iki.fi>
2026-05-23 11:21:33 -1000

    First stab at installing zenburn

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,5 +1,8 @@
 vim.opt.relativenumber = true
+vim.opt.termguicolors = true
 
 local opts = { noremap = true, silent = true }
 
 vim.keymap.set("i", "jj", "<ESC>", opts)
+
+require("zenburn").setup()
