55b845ffa0be9e096624532b22ff367bd1eeb8e6
Henri Bergius <henri.bergius@iki.fi>
2026-07-30 13:16:55 -1000

    neovim LSP

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -31,3 +31,29 @@ vim.keymap.set("i", "<left>", "<nop>", opts)
 vim.keymap.set("i", "<right>", "<nop>", opts)
 
 vim.cmd([[colorscheme tokyonight]])
+
+-- LSP functionality
+vim.lsp.config['vtsls'] = {
+  cmd = { 'vtsls', '--stdio' },
+  filetypes = { 'javascript', 'typescript' },
+  root_markers = { 'package-lock.json', 'yarn.lock', 'pnpm-lock.yaml', 'bun.lockb', 'bun.lock' },
+}
+vim.lsp.enable('vtsls')
+vim.diagnostic.config({
+    virtual_lines = {
+        current_line = true,
+    },
+})
+vim.api.nvim_create_autocmd('LspAttach', {
+  callback = function(args)
+    local bufnr = args.buf
+    local map = function(mode, lhs, rhs, desc)
+      vim.keymap.set(mode, lhs, rhs, { buffer = bufnr, desc = desc })
+    end
+
+    map('n', 'K', vim.lsp.buf.hover, 'LSP Hover')
+    map('n', 'gd', vim.lsp.buf.definition, 'Go to definition')
+    map('n', 'gi', vim.lsp.buf.implementation, 'Go to implementation')
+    map('n', 'gr', vim.lsp.buf.references, 'References')
+  end,
+})
